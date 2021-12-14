62e31d4c6d3c70cbfd58daef9aea0fed6e31393a
Lyndon Forsythe <lyndon@rainchild.com>
2021-12-15 00:51:06 +1300

    Removed unused namespaces from Days 5 and 6

--- Assets/Scripts/Puzzles/Day6.cs
+++ Assets/Scripts/Puzzles/Day6.cs
@@ -1,10 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using NaughtyAttributes;
 using UnityEngine;
-using Unity.EditorCoroutines.Editor;
-using UnityEditor;
 
 public class Day6 : PuzzleBase
 {
