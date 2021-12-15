2db0c39dae058e3d0dc5929e6f4598c65cdb4b49
Lyndon Forsythe <lyndon@rainchild.com>
2021-12-15 22:59:24 +1300

    Implement Day 8, Puzzle 2

--- Assets/Scripts/Puzzles/Day8.cs
+++ Assets/Scripts/Puzzles/Day8.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class Day8 : PuzzleBase
@@ -47,10 +48,10 @@ public class Day8 : PuzzleBase
 
 			Dictionary<string, int> stringToDigitMapping = new Dictionary<string, int>
 			{
-				{ FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[1]), 1 },
-				{ FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[4]), 4 },
-				{ FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[7]), 7 },
-				{ FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[8]), 8 },
+				{ OrderString(FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[1])), 1 },
+				{ OrderString(FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[4])), 4 },
+				{ OrderString(FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[7])), 7 },
+				{ OrderString(FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[8])), 8 },
 			};
 
 			totalKnownDigits += puzzleDigitStrings.Count(digitString => stringToDigitMapping.ContainsKey(OrderString(digitString)));
@@ -69,15 +70,116 @@ public class Day8 : PuzzleBase
 		List<string> validDigitStrings = digitStrings.Where(str => str.Length == segmentCount).ToList();
 		if (validDigitStrings.Count == 1)
 		{
-			return OrderString(validDigitStrings[0]);
+			return validDigitStrings[0];
 		}
 
 		LogError("Found " + validDigitStrings.Count + " digits with " + segmentCount + " segments");
 		return string.Empty;
 	}
 
+	//  dddd
+	// e    a
+	// e    a
+	//  ffff
+	// g    b
+	// g    b
+	//  cccc
 	protected override void ExecutePuzzle2()
 	{
+		int sum = 0;
+		foreach (string line in _inputDataLines)
+		{
+			string[] lineData = SplitString(line, " | ");
+			string[] uniqueDigitStrings = SplitString(lineData[0], " ");	// One of each of the ten digits
+			string[] puzzleDigitStrings = SplitString(lineData[1], " ");	// The four digit code to solve
+
+			Dictionary<string, int> stringToDigitMapping = CalculateStringToDigitMapping(uniqueDigitStrings);
+
+			StringBuilder convertedDigits = new StringBuilder();
+			foreach (string digitString in puzzleDigitStrings.Select(OrderString))
+			{
+				if (stringToDigitMapping.TryGetValue(digitString, out int digit))
+				{
+					convertedDigits.Append(digit);
+				}
+				else
+				{
+					LogError("Mapping doesn't contain digit string", digitString);
+				}
+			}
+
+			if (int.TryParse(convertedDigits.ToString(), out int value))
+			{
+				sum += value;
+			}
+			else
+			{
+				LogError("Converted digits could not be parsed as int", convertedDigits.ToString());
+			}
+		}
+
+		LogResult("Sum of converted values", sum);
+	}
+
+	private Dictionary<string, int> CalculateStringToDigitMapping(string[] uniqueDigitStrings)
+	{
+		// Use the four unique digits (1, 4, 7, 8) as a starting point
+		string digit1 = FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[1]);
+		string digit4 = FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[4]);
+		string digit7 = FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[7]);
+		string digit8 = FindDigitStringWithUniqueSegmentCount(uniqueDigitStrings, _segmentCountPerDigit[8]);
 		
+		List<string> digitStringsWithFiveSegments = uniqueDigitStrings.Where(digitString => digitString.Length == 5).ToList();
+		List<string> digitStringsWithSixSegments = uniqueDigitStrings.Where(digitString => digitString.Length == 6).ToList();
+		
+		// Find the remaining digits (0, 2, 3, 5, 6, 9)
+		// 9 - Has six segments, includes all segments that belong to 4
+		string digit9 = digitStringsWithSixSegments.First(digitString => ContainsAllSegmentsFromDigit(digitString, digit4));
+		digitStringsWithSixSegments.Remove(digit9);
+
+		// 0 - Has six segments, includes all segments that belong to 1  
+		string digit0 = digitStringsWithSixSegments.First(digitString => ContainsAllSegmentsFromDigit(digitString, digit1));
+		digitStringsWithSixSegments.Remove(digit0);
+		
+		// 6 - The other digit with six segments
+		string digit6 = digitStringsWithSixSegments[0];
+		
+		// 3 - Has five segments, includes all segments that belong to 1
+		string digit3 = digitStringsWithFiveSegments.First(digitString => ContainsAllSegmentsFromDigit(digitString, digit1));
+		digitStringsWithFiveSegments.Remove(digit3);
+		
+		// 5 - Has five segments, all of which fit into 6 and/or 9
+		string digit5 = digitStringsWithFiveSegments.First(digitString => ContainsAllSegmentsFromDigit(digit6, digitString));	// Parameters are backwards on this one!
+		digitStringsWithFiveSegments.Remove(digit5);
+		
+		// 2 - The remaining digit
+		string digit2 = digitStringsWithFiveSegments[0];
+
+		return new Dictionary<string, int>
+		{
+			{ OrderString(digit0), 0 },
+			{ OrderString(digit1), 1 },
+			{ OrderString(digit2), 2 },
+			{ OrderString(digit3), 3 },
+			{ OrderString(digit4), 4 },
+			{ OrderString(digit5), 5 },
+			{ OrderString(digit6), 6 },
+			{ OrderString(digit7), 7 },
+			{ OrderString(digit8), 8 },
+			{ OrderString(digit9), 9 },
+		};
+	}
+	
+	private bool ContainsAllSegmentsFromDigit(string thisDigit, string otherDigit)
+	{
+		foreach (char c in otherDigit)
+		{
+			if (!thisDigit.Contains(c))
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
