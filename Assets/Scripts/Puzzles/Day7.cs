71520e3c0b330d662c4f509985f3b0774ff34e3b
Lyndon Forsythe <lyndon@rainchild.com>
2021-12-15 16:45:33 +1300

    Implement Day 7, Puzzle 2

--- Assets/Scripts/Puzzles/Day7.cs
+++ Assets/Scripts/Puzzles/Day7.cs
@@ -29,6 +29,32 @@ public class Day7 : PuzzleBase
 
 	protected override void ExecutePuzzle2()
 	{
-		
+		int lowestFuelCost = int.MaxValue;
+		int bestPosition = -1;
+		int[] crabPositions = ParseIntArray(SplitString(_inputDataLines[0], ","));
+		for (int checkPosition = Mathf.Min(crabPositions); checkPosition < Mathf.Max(crabPositions); checkPosition++)
+		{
+			int totalFuelCost = 0;
+			foreach (int crabPosition in crabPositions)
+			{
+				int distance = Mathf.Abs(crabPosition - checkPosition);
+				int fuelCost = 0;
+				for (int d = 1; d <= distance; d++)
+				{
+					fuelCost += d;
+				}
+				
+				totalFuelCost += fuelCost;
+			}
+
+			if (totalFuelCost < lowestFuelCost)
+			{
+				lowestFuelCost = totalFuelCost;
+				bestPosition = checkPosition;
+			}
+		}
+
+		LogResult("Best position", bestPosition);
+		LogResult("Total fuel cost", lowestFuelCost);
 	}
 }
