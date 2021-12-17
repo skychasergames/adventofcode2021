29173c6f7e607f1f74d72ca2a7fc3cedbf8a97e3
Lyndon Forsythe <lyndon@rainchild.com>
2021-12-18 00:42:59 +1300

    Implement Day 10, Puzzle 2

--- Assets/Scripts/Puzzles/Day10.cs
+++ Assets/Scripts/Puzzles/Day10.cs
@@ -32,9 +32,19 @@ public class Day10 : PuzzleBase
 		{ '}', 1197 },
 		{ '>', 25137 },
 	};
+
+	private readonly Dictionary<char, int> _autocompleteScore = new Dictionary<char, int>
+	{
+		{ ')', 1 },
+		{ ']', 2 },
+		{ '}', 3 },
+		{ '>', 4 },
+	};
 	
 	protected override void ExecutePuzzle1()
 	{
+		ClearText();
+
 		StringBuilder displayStringBuilder = new StringBuilder();
 		
 		int totalSyntaxErrorScore = 0;
@@ -44,7 +54,7 @@ public class Day10 : PuzzleBase
 			ValidateLine(line,
 				
 				// Valid line callback
-				(isLineComplete) =>
+				(isLineComplete, closingCharsNeededIfIncomplete) =>
 				{
 					Log("Line " + i + " is valid (" + (isLineComplete ? "complete" : "incomplete") + ")");
 					
@@ -76,7 +86,7 @@ public class Day10 : PuzzleBase
 		LogResult("Total syntax error score", totalSyntaxErrorScore);
 	}
 
-	private void ValidateLine(string line, Action<bool> validLineCallback, Action<char, char, int> corruptedLineCallback)
+	private void ValidateLine(string line, Action<bool, string> validLineCallback, Action<char, char, int> corruptedLineCallback)
 	{
 		Stack<char> openChunks = new Stack<char>();
 		for (int i = 0; i < line.Length; i++)
@@ -105,13 +115,86 @@ public class Day10 : PuzzleBase
 				}
 			}
 		}
-		
+
 		// Line was valid!
-		validLineCallback(openChunks.Count == 0);
+		bool isLineComplete = openChunks.Count == 0;
+		
+		// Finish it if incomplete
+		StringBuilder closingCharsNeededIfIncomplete = new StringBuilder();
+		while (openChunks.Count > 0)
+		{
+			char openingChar = openChunks.Pop();
+			char closingChar = _chunkPairs[openingChar];
+			closingCharsNeededIfIncomplete.Append(closingChar);
+		}
+
+		validLineCallback(isLineComplete, closingCharsNeededIfIncomplete.ToString());
 	}
 
 	protected override void ExecutePuzzle2()
 	{
+		ClearText();
 		
+		StringBuilder displayStringBuilder = new StringBuilder();
+		
+		List<ulong> lineScores = new List<ulong>();
+		for (int i = 0; i < _inputDataLines.Length; i++)
+		{
+			string line = _inputDataLines[i];
+			ValidateLine(line,
+				
+				// Valid line callback
+				(isLineComplete, closingCharsNeededIfIncomplete) =>
+				{
+					Log("Line " + i + " is valid (" + (isLineComplete ? "complete" : "incomplete") + ")");
+
+					StringBuilder lineDisplayStringBuilder = new StringBuilder(line);
+					
+					if (!isLineComplete)
+					{
+						lineDisplayStringBuilder.Append("<color=green>")
+							.Append(closingCharsNeededIfIncomplete)
+							.Append("</color>");
+						
+						line += closingCharsNeededIfIncomplete;
+
+						ulong lineScore = 0;
+						foreach (char c in closingCharsNeededIfIncomplete)
+						{
+							lineScore *= 5;
+							lineScore += (ulong)_autocompleteScore[c];
+						}
+						
+						lineScores.Add(lineScore);
+					}
+					
+					displayStringBuilder.AppendLine(lineDisplayStringBuilder.ToString());
+				},
+				
+				// Corrupted line callback
+				(illegalChar, expectedChar, illegalCharIndex) =>
+				{
+					Log("Line " + i + " encountered illegal character `" + illegalChar + "' (expected `" + expectedChar + "')");
+					
+					StringBuilder lineDisplayStringBuilder = new StringBuilder()
+						.Append("<color=grey>")
+						.Append(line.Substring(0, illegalCharIndex))
+						.Append("</color><color=red>")
+						.Append(illegalChar)
+						.Append("</color><color=grey>")
+						.Append(line.Substring(illegalCharIndex + 1))
+						.Append("</color>");
+					displayStringBuilder.AppendLine(lineDisplayStringBuilder.ToString());
+				}
+			);
+		}
+
+		_textMesh.SetText(displayStringBuilder);
+
+		lineScores.Sort();
+		
+		int middleIndex = Mathf.FloorToInt(lineScores.Count / 2f);
+		ulong middleScore = lineScores[middleIndex];
+		LogResult("Middle autocorrect score", middleScore);
 	}
 }
