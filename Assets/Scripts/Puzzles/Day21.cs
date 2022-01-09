7cb307a7083940149de9f113fb75ac4873af0ed9
Lyndon Forsythe <lyndon@rainchild.com>
2022-01-09 21:03:59 +1300

    Implement Day 21, Puzzle 2, though I can't really claim credit for it :grimace:

--- Assets/Scripts/Puzzles/Day21.cs
+++ Assets/Scripts/Puzzles/Day21.cs
@@ -1,48 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NaughtyAttributes;
-using Unity.EditorCoroutines.Editor;
-using UnityEditor;
 using UnityEngine;
 
 public class Day21 : PuzzleBase
 {
-	[SerializeField] private int _scoreToWin = 1000;
-	[SerializeField] private float _playerInterval = 2f;
+	[SerializeField] private int _scoreToWinPuzzle1 = 1000;
+	[SerializeField] private int _scoreToWinPuzzle2 = 21;
 	[SerializeField] private int _boardLength = 10;
-	[SerializeField] private int _deterministicDieSides = 100;
 	[SerializeField] private int _rollsPerPlayerTurn = 3;
-	
-	private EditorCoroutine _executePuzzleCoroutine = null;
-
-	[Button]
-	private void ResetBoard()
-	{
-		if (_executePuzzleCoroutine != null)
-		{
-			EditorCoroutineUtility.StopCoroutine(_executePuzzleCoroutine);
-		}
-	}
+	[SerializeField] private int _puzzle1DieSides = 100;
+	[SerializeField] private int _puzzle2DieSides = 3;
 	
 	protected override void ExecutePuzzle1()
 	{
-		// Initialize
-		ResetBoard();
-
-		IDie die = new DeterministicDie(_deterministicDieSides);
-		_executePuzzleCoroutine = EditorCoroutineUtility.StartCoroutine(ExecutePuzzle(die), this);
-	}
-
-	private IEnumerator ExecutePuzzle(IDie die)
-	{
-		EditorWaitForSeconds playerInterval = new EditorWaitForSeconds(_playerInterval);
+		IDie die = new DeterministicDie(_puzzle1DieSides);
 		
-		EditorApplication.QueuePlayerLoopUpdate();
-		yield return playerInterval;
-
 		int player1StartPos = int.Parse(SplitString(_inputDataLines[0], ":")[1]);
 		int player2StartPos = int.Parse(SplitString(_inputDataLines[1], ":")[1]);
 		Player player1 = new Player(1, player1StartPos);
@@ -72,7 +44,7 @@ public class Day21 : PuzzleBase
 			currentPlayer.StopAtPosition(newPosition);
 			Log("Player " + currentPlayer.PlayerNumber + " moved to " + newPosition + ". Current score: " + currentPlayer.Score);
 
-			if (currentPlayer.Score >= _scoreToWin)
+			if (currentPlayer.Score >= _scoreToWinPuzzle1)
 			{
 				Log("Game ended after " + totalRolls + " rolls!");
 				Log("Player " + currentPlayer.PlayerNumber + " won with score " + currentPlayer.Score);
@@ -83,13 +55,9 @@ public class Day21 : PuzzleBase
 				break;
 			}
 
-			yield return playerInterval;
-			
 			otherPlayer = currentPlayer;
 			currentPlayer = (currentPlayer == player1 ? player2 : player1);
 		}
-		
-		_executePuzzleCoroutine = null;
 	}
 
 	private class Player
@@ -141,6 +109,52 @@ public class Day21 : PuzzleBase
 
 	protected override void ExecutePuzzle2()
 	{
-		
+		int player1StartPos = int.Parse(SplitString(_inputDataLines[0], ":")[1]);
+		int player2StartPos = int.Parse(SplitString(_inputDataLines[1], ":")[1]);
+
+		gameStates.Clear();
+		(ulong, ulong) result = CalculateGameState(player1StartPos-1, player2StartPos-1, 0, 0);
+
+		LogResult("Player 1 wins", result.Item1);
+		LogResult("Player 2 wins", result.Item2);
+	}
+
+	// Day 21 beat me. Honestly, I don't get any of this :sadge:
+	// All credit goes to Jonathan Paulson (https://www.youtube.com/watch?v=a6ZdJEntKkk)
+	private Dictionary<(int, int, int, int), (ulong, ulong)> gameStates = new Dictionary<(int, int, int, int), (ulong, ulong)>();
+	private (ulong, ulong) CalculateGameState(int player1Pos, int player2Pos, int player1Score, int player2Score)
+	{
+		if (player1Score >= _scoreToWinPuzzle2)
+		{
+			return (1, 0);
+		}
+		if (player2Score >= _scoreToWinPuzzle2)
+		{
+			return (0, 1);
+		}
+
+		if (gameStates.TryGetValue((player1Pos, player2Pos, player1Score, player2Score), out (ulong, ulong) gameStateCount))
+		{
+			return gameStateCount;
+		}
+
+		(ulong, ulong) newGameState = (0, 0); 
+		for (int roll1 = 1; roll1 <= _puzzle2DieSides; roll1++)
+		{
+			for (int roll2 = 1; roll2 <= _puzzle2DieSides; roll2++)
+			{
+				for (int roll3 = 1; roll3 <= _puzzle2DieSides; roll3++)
+				{
+					int newPlayer1Pos = (player1Pos + roll1 + roll2 + roll3) % 10;
+					int newPlayer2Pos = player1Score + newPlayer1Pos + 1;
+
+					(ulong, ulong) recursiveGameState = CalculateGameState(player2Pos, newPlayer1Pos, player2Score, newPlayer2Pos);
+					newGameState = (newGameState.Item1 + recursiveGameState.Item2, newGameState.Item2 + recursiveGameState.Item1);
+				}
+			}
+		}
+
+		gameStates.Add((player1Pos, player2Pos, player1Score, player2Score), newGameState);
+		return newGameState;
 	}
 }
