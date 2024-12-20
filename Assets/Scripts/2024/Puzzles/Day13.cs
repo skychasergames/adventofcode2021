7775148888fd405d34f618130d8bb92c64eb3491
Lyndon Forsythe <lyndon@skychasergames.com>
2024-12-20 22:39:24 +1300

    Implement AoC2024 Day 13, Puzzle 2

--- Assets/Scripts/2024/Puzzles/Day13.cs
+++ Assets/Scripts/2024/Puzzles/Day13.cs
@@ -12,6 +12,7 @@ namespace AoC2024
 		private const int BUTTON_A_COST = 3;
 		private const int BUTTON_B_COST = 1;
 		private const int MAX_PRESSES_PER_BUTTON = 100;
+		private const long UNIT_CONVERSION_ERROR = 10000000000000;
 
 		protected override void ExecutePuzzle1()
 		{
@@ -36,7 +37,65 @@ namespace AoC2024
 
 		protected override void ExecutePuzzle2()
 		{
+			long totalTokenCost = 0;
 			
+			for (int line = 0; line < _inputDataLines.Length; line += 3)
+			{
+				// I'm so bad at maths :notlikethis:
+				
+				// Solve as a system of equations
+				ParseClawMachine(line, out Vector2Int buttonAMovement, out Vector2Int buttonBMovement, out Vector2Int prizePosition);
+				long buttonAMovementX = buttonAMovement.x;
+				long buttonAMovementY = buttonAMovement.y;
+				long buttonBMovementX = buttonBMovement.x;
+				long buttonBMovementY = buttonBMovement.y;
+				long prizePositionX = prizePosition.x + UNIT_CONVERSION_ERROR;
+				long prizePositionY = prizePosition.y + UNIT_CONVERSION_ERROR;
+
+				// <Original equations>
+				// buttonAMovementX + buttonBMovementX = prizePositionX
+				// buttonAMovementY + buttonBMovementY = prizePositionY
+				
+				// <Eliminate Button B>
+				//  buttonBMovementY * (buttonAMovementX + buttonBMovementX) =  buttonBMovementY * prizePositionX
+				// -buttonBMovementX * (buttonAMovementY + buttonBMovementY) = -buttonBMovementX * prizePositionY
+				
+				//  buttonBMovementY * buttonAMovementX =  buttonBMovementY * prizePositionX
+				// -buttonBMovementX * buttonAMovementY = -buttonBMovementX * prizePositionY
+				
+				// <Add the equations>
+				//  buttonBMovementY * buttonAMovementX + -buttonBMovementX * buttonAMovementY = (buttonBMovementY * prizePositionX + -buttonBMovementX * prizePositionY)
+				
+				// <Solve for buttonAPresses>
+				// buttonAPresses = (buttonBMovementY * prizePositionX + -buttonBMovementX * prizePositionY) / (buttonBMovementY * buttonAMovementX + -buttonBMovementX * buttonAMovementY)
+				
+				double buttonAPressesFractional = (double)(buttonBMovementY * prizePositionX + -buttonBMovementX * prizePositionY) / (buttonBMovementY * buttonAMovementX + -buttonBMovementX * buttonAMovementY);
+
+				// <If buttonAPresses is a fraction, then there's no valid solution for this claw machine!>
+				if (Math.Abs(buttonAPressesFractional - (long)buttonAPressesFractional) > 0.0001)
+				{
+					continue;
+				}
+				
+				long buttonAPresses = (long)buttonAPressesFractional;
+				
+				// <Plug buttonAPresses back into one of the original equations to get buttonBPresses>
+				// buttonAMovementX + buttonBMovementX = prizePositionX
+				
+				// buttonAMovementX * buttonAPresses + buttonBMovementX = prizePositionX
+				
+				// buttonBMovementX = prizePositionX - buttonAMovementX * buttonAPresses
+				
+				// buttonBPresses = (prizePositionX - buttonAMovementX * buttonAPresses) / buttonBMovementX
+				
+				long buttonBPresses = (prizePositionX - buttonAMovementX * buttonAPresses) / buttonBMovementX;
+
+				long tokenCost = BUTTON_A_COST * buttonAPresses + BUTTON_B_COST * buttonBPresses;
+				Log("Found prize for " + tokenCost + " tokens (" + buttonBPresses + " presses of Button B)");
+				totalTokenCost += tokenCost;
+			}
+			
+			LogResult("Total token cost", totalTokenCost);
 		}
 
 		private void ParseClawMachine(int line, out Vector2Int buttonAMovement, out Vector2Int buttonBMovement, out Vector2Int prizePosition)
